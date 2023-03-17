9ab434a338d59a1db6cf96a312ca17c7bbeb924e
Mohamed Said Ben Ali <mohamedsaidbenali2428@gmail.com>
2023-03-18 00:54:30 +0100

    said changes

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -10,34 +10,20 @@ export class LoginComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-  const sign_in_btn = document.querySelector("#sign-in-btn");
-  const sign_up_btn = document.querySelector("#sign-up-btn");
-  const container = document.querySelector(".container");
-  
-<<<<<<< HEAD
-  if (sign_up_btn) {
-    sign_up_btn.addEventListener("click", () => {
-      if (container) {
-        container.classList.add("sign-up-mode");
-      }
+    const signUpButton = document.getElementById('signUp');
+    const signInButton = document.getElementById('signIn');
+    const container = document.getElementById('container');
+    
+    signUpButton?.addEventListener('click', () => {
+      container?.classList.add("right-panel-active");
     });
-  }
-  
-  if (sign_in_btn) {
-    sign_in_btn.addEventListener("click", () => {
-      if (container) {
-        container.classList.remove("sign-in-mode");
-      }
+    
+    signInButton?.addEventListener('click', () => {
+      container?.classList.remove("right-panel-active");
     });
-  }
   }}
-=======
-  sign_up_btn?.addEventListener("click", () => {
-    container?.classList.add("sign-up-mode");
-  });
-  sign_in_btn?.addEventListener("click", () => {
-    container?.classList.remove("sign-in-mode");
-  });
-}
-}
->>>>>>> 2f582351e1ee6d6954034980a8621710f4d76b99
+
+ 
+
+
+

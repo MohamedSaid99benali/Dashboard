708a06ce746a0d5e2dfdbc2c9430294037dbb3fd
Mohamed Said Ben Ali <mohamedsaidbenali2428@gmail.com>
2023-03-18 01:05:18 +0100

    khra changes

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -10,15 +10,16 @@ export class LoginComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-  const sign_in_btn = document.querySelector("#sign-in-btn");
-  const sign_up_btn = document.querySelector("#sign-up-btn");
-  const container = document.querySelector(".container");
-  
-  sign_up_btn?.addEventListener("click", () => {
-    container?.classList.add("sign-up-mode");
-  });
-  sign_in_btn?.addEventListener("click", () => {
-    container?.classList.remove("sign-in-mode");
-  });
+    const signUpButton = document.getElementById('signUp');
+    const signInButton = document.getElementById('signIn');
+    const container = document.getElementById('container');
+    
+    signUpButton?.addEventListener('click', () => {
+      container?.classList.add("right-panel-active");
+    });
+    
+    signInButton?.addEventListener('click', () => {
+      container?.classList.remove("right-panel-active");
+    });
 }
 }
